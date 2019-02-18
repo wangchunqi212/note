e2955293d4fc44b28365635ab58774005f620a85
yh6567 <381710738@qq.com>
2019-02-18 21:19:52 +0800

    yuan

---
+++ myapp/src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore,applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {combineReducers} from "redux-immutable";
+import a from "./reducers/";
+import b from "./reducers/b";
+
+
+const reducers = combineReducers({
+    a,
+    b
+})
+
+const store = createStore(reducers,applyMiddleware(thunk));
+
+export default store;
